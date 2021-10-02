bb69956e811ef9d7cfb35d70b9790babacb80b3c
Sky-Macbook <parkparkhaneul@gmail.com>
2021-10-02 13:55:02 -0700

    added Bishop moving feature, consider when blocked (piece on diagonal)

---
+++ Chess/src/Test/BishopTest.java
@@ -0,0 +1,42 @@
+package Test;
+
+import main.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BishopTest {
+
+    private Game g;
+    private ChessBoard cb;
+    private List<Position> bd;
+
+    @BeforeEach
+    void setUp() {
+        g = new Game();
+        cb = g.getChessBoard();
+        bd = cb.getBoard();
+    }
+
+    void testMoveHelper(Piece p, int setNp, int moveNp, String expected) {
+        bd.get(setNp).setPiece(p);
+        bd.get(setNp).getPiece().move(bd.get(moveNp), cb);
+
+        // check if the knight was removed from the original position
+        assertEquals(null, bd.get(setNp).getPiece());
+        // check if the pawn was moved (position has piece)
+        assertEquals(expected, bd.get(moveNp).getPiece().getName());
+        // check if the pawn was moved (piece has position)
+        assertEquals(moveNp+1, bd.get(moveNp).getPiece().getPosition().getNumberPosition());
+
+    }
+
+    @Test
+    void WhiteBishopMove() {
+        testMoveHelper(new Bishop("WB"), 27, 34, "WB");
+
+    }
+}
--- Chess/src/main/Bishop.java
+++ Chess/src/main/Bishop.java
@@ -9,8 +9,55 @@ public class Bishop extends Piece {
     }
 
     @Override
-    public void move(Position p, ChessBoard cb) {
-        int i = 1;
+    public void move(Position pos, ChessBoard cb) {
+
+        int moveToNumberPos = pos.getNumberPosition();// numberPosition that we are moving to
+        Position currentPos = this.position;//position current
+        int currentNumberPos = this.position.getNumberPosition();// numberPosition current
+
+
+        if (moveToNumberPos >= 64 || moveToNumberPos <= -1) { // out of range case // can be defined as a helper
+            System.out.print("out of bound");// possible throw exception
+        } if (isInRange(moveToNumberPos,currentNumberPos)) {
+            if (isWhite(this)) {//white
+                if (pos.getPiece() == null) {// if front empty // helper??? //simpiify using pos.get...
+                    currentPos.removePiece();// should remove from current position
+                    pos.setPiece(this); // can be defined as a helper
+                } else if (!isWhite(pos.getPiece())) {
+                    currentPos.removePiece();// should remove from current position
+                    pos.setPiece(this); // can be defined as a helper
+                } else {
+                    //teamkillexception
+                }
+            } else {//black
+                if (pos.getPiece() == null) {// if front empty // helper??? //simpiify using pos.get...
+                    currentPos.removePiece();// should remove from current position
+                    pos.setPiece(this); // can be defined as a helper
+                } else if (isWhite(pos.getPiece())) {
+                    currentPos.removePiece();// should remove from current position
+                    pos.setPiece(this); // can be defined as a helper
+                } else {
+                    //teamkillexception
+                }
+
+            }
+        } else {
+            System.out.print("not valid position");
+
+
+        }
+
+    }
+
+    boolean isInRange(int moveToNumberPos, int currentNumberPos) {
+
+        for (int n = -7; n < 8; n++) {
+            if ((moveToNumberPos == currentNumberPos + 8 * n + n)
+                    || moveToNumberPos == currentNumberPos + 8 * n - n) {
+                return true;
+            }
+        }
+        return false;
 
     }
 }
