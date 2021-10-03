fb94d18f7752b0381ec9aed419c54380f9498e30
Sky-Macbook <parkparkhaneul@gmail.com>
2021-10-02 19:46:22 -0700

    i fucked it up fix them

--- Chess/src/Test/BishopTest.java
+++ Chess/src/Test/BishopTest.java
@@ -31,7 +31,22 @@ public class BishopTest {
         assertEquals(expected, bd.get(moveNp).getPiece().getName());
         // check if the pawn was moved (piece has position)
         assertEquals(moveNp+1, bd.get(moveNp).getPiece().getPosition().getNumberPosition());
+    }
 
+    void testKillHelper(Piece p, int setNp, int moveNp, String expected, Piece expectedToKill) {
+        // place White knight at index 35 (for move in every possible position)
+        bd.get(setNp).setPiece(p);
+        //
+        bd.get(moveNp).setPiece(expectedToKill);
+        // move Knight to index 50 // killing
+        bd.get(setNp).getPiece().move(bd.get(moveNp),cb);
+
+        // check if the knight was removed from the original position
+        assertEquals(null,bd.get(setNp).getPiece());
+        // check if the pawn was moved (position has piece)
+        assertEquals(expected,bd.get(moveNp).getPiece().getName());
+        // check if the pawn was moved (piece has position)
+        assertEquals(moveNp+1,bd.get(moveNp).getPiece().getPosition().getNumberPosition());
     }
 
     @Test
@@ -45,6 +60,11 @@ public class BishopTest {
     }
 
     @Test
+    void WhiteBishopKillNW3() {
+        testKillHelper(new Bishop("WB"), 27, 41, "WB", new Bishop("BB"));
+    }
+
+    @Test
     void WhiteBishopMoveNE1() { // northWest
         testMoveHelper(new Bishop("WB"), 27, 36, "WB");
     }
@@ -54,19 +74,44 @@ public class BishopTest {
         testMoveHelper(new Bishop("WB"), 27, 45, "WB");
     }
 
-//    @Test
-//    void WhiteBishopKillNE3() {
-//
-//    }
+    @Test
+    void WhiteBishopKillNE3() {
+        testKillHelper(new Bishop("WB"), 27, 54, "WB", new Bishop("BB"));
+    }
 
     @Test
     void WhiteBishopMoveSW1() { // northWest
         testMoveHelper(new Bishop("WB"), 27, 18, "WB");
     }
 
+
     @Test
     void WhiteBishopMoveSE1() { // northWest
         testMoveHelper(new Bishop("WB"), 27, 20, "WB");
+    }
+
+    @Test
+    void TeamKillTest() {
+        bd.get(27).setPiece(new Bishop("WB"));
+        // move Knight to index 50 // killing
+        bd.get(27).getPiece().move(bd.get(9),cb);
+
+        // check if the knight was removed from the original position
+        assertEquals("WB",bd.get(27).getPiece().getName());
+        // check if the pawn was moved (position has piece)
+        assertEquals("WP",bd.get(9).getPiece().getName());
+    }
+
+    @Test
+    void blockTestNW() {
+        bd.get(27).setPiece(new Bishop("WB"));
+        bd.get(34).setPiece(new Bishop("WB"));
+        // move Knight to index 50 // killing
+        bd.get(27).getPiece().move(bd.get(41),cb);
+
+        assertEquals("WB",bd.get(27).getPiece().getName());
+        assertEquals(null,bd.get(41).getPiece());
+
     }
 
 
--- Chess/src/main/Bishop.java
+++ Chess/src/main/Bishop.java
@@ -20,7 +20,7 @@ public class Bishop extends Piece {
             System.out.print("out of bound");// possible throw exception
         } else {
             if (isInRange(moveToNumberPos,currentNumberPos)) {
-                if (isNoBlocking(currentNumberPos,moveToNumberPos,cb)) {
+                if (!isBlocking(currentNumberPos,moveToNumberPos,cb)) {
                     if (isWhite(this)) {//white
                         if (pos.getPiece() == null) {// if front empty // helper??? //simpiify using pos.get...
                             currentPos.removePiece();// should remove from current position
@@ -63,65 +63,69 @@ public class Bishop extends Piece {
 
     }
 
-    boolean isNoBlocking(int curr, int moveTo, ChessBoard cb) {
+    boolean isBlocking(int curr, int moveTo, ChessBoard cb) {
         String dir = "";
-        System.out.print("in isnoblock");
         for (int n = -7; n < 8; n++) {
             if (moveTo == curr + n*8 + n) {
-                System.out.print("in here");
                 if (n > 0) {
                     dir = "NE";
+                    break;
 
                 } else if (n == 0) {
                     dir = "NONE";
+                    break;
                 } else {
                     dir = "SE";
+                    break;
                 }
             } else if(moveTo == curr + n*8 - n) {
                 if (n > 0) {
                     dir = "NW";
+                    break;
                 } else if (n==0) {
                     dir = "NONE";
+                    break;
                 } else {
                     dir = "SW";
+                    break;
                 }
             }
         }
         System.out.print(dir);
-
+//since curr is numberposition(which is one larger than the coordinate, subtracting 1
         if (dir.equals("NE")) {
             for (int i = curr; i < moveTo; i = i+9) {
-                if (!(cb.getBoard().get(curr).getPiece() == null)) {
-                    return false;
+                if (!(cb.getBoard().get(i-1).getPiece() == null)) {
+                    return true;
                 }
             }
-            return true;
+            return false;
         } else if (dir.equals("SE")) {
             for (int i = curr; i < moveTo; i = i-7) {
-                if (!(cb.getBoard().get(curr).getPiece() == null)) {
-                    return false;
+                if (!(cb.getBoard().get(i-1).getPiece() == null)) {
+                    return true;
                 }
             }
-            return true;
+            return false;
         } else if (dir.equals("NW")) {
             for (int i = curr; i < moveTo; i = i+7) {
-                if (!(cb.getBoard().get(curr).getPiece() == null)) {
-                    return false;
+                if (!(cb.getBoard().get(i-1).getPiece() == null)) {
+                    return true;
                 }
             }
-            return true;
+            return false;
 
         } else if (dir.equals("SW")) {
             for (int i = curr; i < moveTo; i = i-9) {
-                if (!(cb.getBoard().get(curr).getPiece() == null)) {
-                    return false;
+                if (!(cb.getBoard().get(i-1).getPiece() == null)) {
+                    return true;
                 }
             }
-            return true;
+            return false;
 
         } else {
             System.out.print("invalid position");//throw exception
-            return false;
+            return true;
 
 
         }
