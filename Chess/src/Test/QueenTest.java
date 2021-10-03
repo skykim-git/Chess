16d2518f26e2dab539bb7fbed3f6baf15f9ce2fb
Sky-Macbook <parkparkhaneul@gmail.com>
2021-10-03 15:27:59 -0700

    Queen copy test from both Rook and Bishop passes! :) Do king

--- Chess/src/Test/QueenTest.java
+++ Chess/src/Test/QueenTest.java
@@ -210,6 +210,217 @@ public class QueenTest {
         assertEquals("BQ",bd.get(59).getPiece().getName());
     }
 
+    @Test
+    void WhiteQueenMoveNW1() { // northWest
+        testMoveHelper(new Queen("WQ"), 27, 34, "WQ");
+    }
+
+    @Test
+    void WhiteQueenMoveNW2() {
+        testMoveHelper(new Queen("WQ"), 27, 41, "WQ");
+    }
+
+    @Test
+    void WhiteQueenKillNW3() {
+        testMoveHelper(new Queen("WQ"), 27, 48, "WQ");
+    }
+
+    @Test
+    void WhiteQueenMoveNE1() { // northWest
+        testMoveHelper(new Queen("WQ"), 27, 36, "WQ");
+    }
+
+    @Test
+    void WhiteQueenMoveNE2() { // northWest
+        testMoveHelper(new Queen("WQ"), 27, 45, "WQ");
+    }
+
+    @Test
+    void WhiteQueenKillNE3() {
+        //testKillHelper(new Queen("WQ"), 27, 54, "WQ", new Queen("BB"));
+    }
+
+    @Test
+    void WhiteQueenCanNotKillNE4() {
+        // place White knight at index 35 (for move in every possible position)
+        bd.get(27).setPiece(new Queen("WQ"));
+        bd.get(27).getPiece().move(bd.get(63),cb);
+
+        // check if the knight was removed from the original position
+        assertEquals("WQ",bd.get(27).getPiece().getName());
+        // check if the pawn was moved (position has piece)
+        assertEquals("BR",bd.get(63).getPiece().getName());
+
+    }
+
+    @Test
+    void WhiteQueenMoveSW1() { // northWest
+        testMoveHelper(new Queen("WQ"), 27, 18, "WQ");
+    }
+
+    @Test
+    void WhiteQueenMoveSW2() {
+        bd.get(27).setPiece(new Queen("WQ"));
+        bd.get(27).getPiece().move(bd.get(9),cb);
+
+        // check if the knight was removed from the original position
+        assertEquals("WQ",bd.get(27).getPiece().getName());
+        // check if the pawn was moved (position has piece)
+        assertEquals("WP",bd.get(9).getPiece().getName());
+    }
+
+    @Test
+    void WhiteQueenMoveSW3() {
+        bd.get(27).setPiece(new Queen("WQ"));
+        bd.get(27).getPiece().move(bd.get(0),cb);
+
+        // check if the knight was removed from the original position
+        assertEquals("WQ",bd.get(27).getPiece().getName());
+        // check if the pawn was moved (position has piece)
+        assertEquals("WR",bd.get(0).getPiece().getName());
+    }
+
+
+    @Test
+    void WhiteQueenMoveSE1() { // northWest
+        testMoveHelper(new Queen("WQ"), 27, 20, "WQ");
+    }
+
+    @Test
+    void WhiteQueenMoveSE2() {
+        bd.get(27).setPiece(new Queen("WQ"));
+        bd.get(27).getPiece().move(bd.get(13),cb);
+
+        // check if the knight was removed from the original position
+        assertEquals("WQ",bd.get(27).getPiece().getName());
+        // check if the pawn was moved (position has piece)
+        assertEquals("WP",bd.get(13).getPiece().getName());
+    }
+
+    @Test
+    void WhiteQueenMoveSE3() {
+        bd.get(27).setPiece(new Queen("WQ"));
+        bd.get(27).getPiece().move(bd.get(6),cb);
+
+        // check if the knight was removed from the original position
+        assertEquals("WQ",bd.get(27).getPiece().getName());
+        // check if the pawn was moved (position has piece)
+        assertEquals("WN",bd.get(6).getPiece().getName());
+    }
+
+
+
+    @Test
+    void WhiteQueenTeamKillTest() {
+        bd.get(27).setPiece(new Queen("WQ"));
+        // move Knight to index 50 // killing
+        bd.get(27).getPiece().move(bd.get(9),cb);
+
+        // check if the knight was removed from the original position
+        assertEquals("WQ",bd.get(27).getPiece().getName());
+        // check if the pawn was moved (position has piece)
+        assertEquals("WP",bd.get(9).getPiece().getName());
+    }
+
+    @Test
+    void BlackQueenTeamKillTest() {
+        bd.get(27).setPiece(new Queen("BB"));
+        // move Knight to index 50 // killing
+        bd.get(27).getPiece().move(bd.get(48),cb);
+
+        // check if the knight was removed from the original position
+        assertEquals("BB",bd.get(27).getPiece().getName());
+        // check if the pawn was moved (position has piece)
+        assertEquals("BP",bd.get(48).getPiece().getName());
+    }
+
+    @Test
+    void blockQueenTestNW() {
+        bd.get(27).setPiece(new Queen("WQ"));
+        bd.get(34).setPiece(new Queen("WQ"));
+        // move Knight to index 50 // killing
+        bd.get(27).getPiece().move(bd.get(41),cb);
+
+        assertEquals("WQ",bd.get(27).getPiece().getName());
+        assertEquals(null,bd.get(41).getPiece());
+
+    }
+
+    @Test
+    void BlackQueenMoveNW1() { // northWest
+        testMoveHelper(new Queen("BQ"), 27, 34, "BQ");
+    }
+
+    @Test
+    void BlackQueenMoveNW2() {
+        testMoveHelper(new Queen("BQ"), 27, 41, "BQ");
+    }
+
+    @Test
+    void BlackQueenMoveNE1() { // northWest
+        testMoveHelper(new Queen("BQ"), 27, 36, "BQ");
+    }
+
+    @Test
+    void BlackQueenMoveNE2() { // northWest
+        testMoveHelper(new Queen("BQ"), 27, 45, "BQ");
+    }
+
+    @Test
+    void BlackQueenTeamKillNE4() {
+        bd.get(27).setPiece(new Queen("BQ"));
+        bd.get(27).getPiece().move(bd.get(63),cb);
+
+        // check if the knight was removed from the original position
+        assertEquals("BQ",bd.get(27).getPiece().getName());
+        // check if the pawn was moved (position has piece)
+        assertEquals("BR",bd.get(63).getPiece().getName());
+    }
+
+
+    @Test
+    void BlackQueenMoveSW1() { // northWest
+        testMoveHelper(new Queen("BQ"), 27, 18, "BQ");
+    }
+
+    @Test
+    void BlackQueenKillSW2() { // northWest
+        testMoveHelper(new Queen("BQ"), 27, 9, "BQ");
+    }
+
+    @Test
+    void BlackQueenMoveSW3() { // fail
+        bd.get(27).setPiece(new Queen("BQ"));
+        bd.get(27).getPiece().move(bd.get(0),cb);
+
+        // check if the knight was removed from the original position
+        assertEquals("BQ",bd.get(27).getPiece().getName());
+        // check if the pawn was moved (position has piece)
+        assertEquals("WR",bd.get(0).getPiece().getName());
+    }
+
+    @Test
+    void BlackQueenMoveSE1() { // northWest
+        testMoveHelper(new Queen("BQ"), 27, 20, "BQ");
+    }
+
+    @Test
+    void BlackQueenKillSE2() { // northWest
+        testMoveHelper(new Queen("BQ"), 27, 13, "BQ");
+    }
+
+    // 6 kill fail
+    @Test
+    void BlackQueenMoveSE3() { // fail
+        bd.get(27).setPiece(new Queen("BQ"));
+        bd.get(27).getPiece().move(bd.get(6),cb);
+
+        // check if the knight was removed from the original position
+        assertEquals("BQ",bd.get(27).getPiece().getName());
+        // check if the pawn was moved (position has piece)
+        assertEquals("WN",bd.get(6).getPiece().getName());
+    }
+
 
 
 }
